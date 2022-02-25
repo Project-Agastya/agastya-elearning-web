6dbbcbbee2a522e371388c6fa47f78e3e8cb8b19
Yokesh Amil <ymyokesh@gmail.com>
2022-02-25 12:48:20 +0530

    Moving the login button to the bottom

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import {
   faFacebookSquare,
@@ -6,6 +6,12 @@ import {
   faYoutubeSquare,
   faTwitterSquare,
 } from '@fortawesome/free-brands-svg-icons';
+import { Observable } from 'rxjs';
+import { CurrentUser } from '../models/user.model';
+import { AuthService } from '../services/auth.service';
+import { MatDialog } from '@angular/material/dialog';
+import { LoginComponent } from '../auth/login/login.component';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -17,8 +23,25 @@ export class FooterComponent implements OnInit {
   insta = faInstagramSquare;
   youtube = faYoutubeSquare;
   twitter = faTwitterSquare;
+  user: Observable<CurrentUser> = null;
+  
+  constructor(
+    public auth: AuthService,
+    public dialog: MatDialog,
+    private router: Router,
+  ) {}
 
-  constructor() {}
+  ngOnInit(): void {
+    this.user = this.auth.currentUser;
+  }
 
-  ngOnInit(): void {}
+  openLogin() {
+    const dialogRef = this.dialog.open(LoginComponent, {
+      width: '250px',
+    });
+  }
+  logout() {
+    this.auth.logout();
+    this.router.navigate(['/landing']);
+  }
 }
